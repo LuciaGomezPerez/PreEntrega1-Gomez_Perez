08250d6a522e8ea67aef113835857a5dd64f983a
lucia gomez <lucytgp@gmail.com>
2023-02-20 18:52:48 -0300

    se modifican mensajes y nombre de un producto

--- js/main.js
+++ js/main.js
@@ -6,25 +6,25 @@ const calcularIva = a => a * 0.75;
 
 //INPUTS CON CONDICIONALES Y CICLOS
 
-let seleccion = prompt("Hola desea adquirir algún producto? ingrese si o no")
+let seleccion = prompt("Hola desea consultar precio de un producto? ingrese si o no")
 
 while (seleccion != "si" && seleccion != "no") {
     alert("Dale no tengo todo el día")
-    seleccion = prompt("Desea comprar algo si o no")
+    seleccion = prompt("Desea consultar por un producto si o no")
 }
 if (seleccion == "si") {
     console.log(productos)
     alert("Aqui tiene una lista de productos")
-    comprar = prompt("Que producto quiere comprar?").toLowerCase()
+    comprar = prompt("Que producto le interesa?").toLowerCase()
 } else if (seleccion == "no") {
     alert("Perfecto, tenga un buen día")
 }
 
-while (comprar !== "dark souls" && comprar !== "elden ring" && comprar !== "signalis" && comprar !== "rimWorld" && comprar !== "god of war" && comprar !== "hogwarts legacy") {
+while (comprar !== "dark souls" && comprar !== "elden ring" && comprar !== "signalis" && comprar !== "rimworld" && comprar !== "god of war" && comprar !== "hogwarts legacy") {
     alert("Por favor elija un producto")
-    comprar = prompt("Que producto quiere comprar?").toLowerCase()
+    comprar = prompt("Que producto le interesa?").toLowerCase()
 }
-if (comprar == "dark souls" || comprar == "elden ring" || comprar == "signalis" || comprar == "rimWorld" || comprar == "god of war" || comprar == "hogwarts legacy") {
+if (comprar == "dark souls" || comprar == "elden ring" || comprar == "signalis" || comprar == "rimworld" || comprar == "god of war" || comprar == "hogwarts legacy") {
     let cantidad = parseInt(prompt("Cuantas unidades quieres?"))
     if (!isNaN(cantidad)) {
         let subtotal = cantidad * productos[comprar].precio;
@@ -32,6 +32,6 @@ if (comprar == "dark souls" || comprar == "elden ring" || comprar == "signalis" 
         console.log("Precio unitario: " + productos[comprar].precio + ", Precio total sin IVA: " + subtotal + ", Precio total con IVA: " + total)
         alert("Gracias vuelva prontos")
     } else {
-        console.log("Flaco ingresa un número")
+        alert("Flaco ingresa un número")
     }
 }
